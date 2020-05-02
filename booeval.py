164d9abd652e6f301e535a4cf0487209d441a55b
The Ape Machine <theapemachine@gmail.com>
2020-05-02 16:37:28 +0200

    Some fixes

--- booeval.py
+++ booeval.py
@@ -3,14 +3,21 @@ import logging
 class BooEval:
 
     def __init__(self, ast):
-        self.ast    = ast
-        self.output = ""
+        self.ast       = ast
+        self.output    = ""
+        self.operators = {'+': 'add'}
 
     def Run(self):
         for node in self.ast:
             print(f"booeval.Run({node})")
-            self.add(node)
 
-    def add(self, node):
-        print(f"booeval.add({self.ast[node]})")
-        self.output = sum(map(int, self.ast[node]))
+            n = node
+
+            if node in self.operators:
+                n = self.operators[node]
+
+            getattr(self, n)(self.ast[node])
+
+    def add(self, values):
+        print(f"booeval.add({values})")
+        self.output = sum(map(int, values))
